dbab83c0e997f244c19a2a0a35c6c54fa59c72bb
BabyDragon003 <golddragon01234@gmail.com>
2022-01-26 17:21:52 +0900

    "2022-01-26 17:21"

--- onft-bridge-smartcontract/tasks/omniCounterIncrementWithParamsV2.js
+++ onft-bridge-smartcontract/tasks/omniCounterIncrementWithParamsV2.js
@@ -3,16 +3,11 @@ const CHAIN_ID = require("../constants/chainIds.json")
 
 module.exports = async function (taskArgs, hre) {
     const dstChainId = CHAIN_ID[taskArgs.targetNetwork]
-    const dstAddr = getDeploymentAddresses(taskArgs.targetNetwork)["OmniCounter"]
-    const omniCounter = await ethers.getContract("OmniCounter")
-    console.log(`[source] omniCounter.address: ${omniCounter.address}`)
-    let tx = await (
-        await omniCounter.incrementCounterWithAdapterParamsV2(
-            dstChainId,
-            dstAddr,
-            taskArgs.gasAmount,
-            taskArgs.airDropEthQty,
-            taskArgs.airDropAddr,
+    console.log(`
+        [destination]: ${getDeploymentAddresses(taskArgs.targetNetwork)}, 
+        [gasAmount]: ${taskArgs.gasAmount}, 
+        [airDropEthQty]: ${taskArgs.airDropEthQty}, 
+        [airDropAddr]: ${taskArgs.airDropAddr}
             { value: ethers.utils.parseEther("1") } // estimate/guess
         )
     ).wait()

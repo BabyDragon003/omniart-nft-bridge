5ab68c80cf6126731eb40205ba59657431d8b829
BabyDragon003 <golddragon01234@gmail.com>
2022-03-16 17:21:52 +0900

    "2022-03-16 17:21"

--- onft-bridge-smartcontract/tasks/pingPongSetTrustedRemote.js
+++ onft-bridge-smartcontract/tasks/pingPongSetTrustedRemote.js
@@ -3,6 +3,8 @@ const { getDeploymentAddresses } = require("../utils/readStatic")
 
 module.exports = async function (taskArgs, hre) {
     const dstChainId = CHAIN_ID[taskArgs.targetNetwork]
-    console.log(`✅ [${hre.network.name}] PingPong.setTrustedRemote( ${dstChainId}, ${dstPingPongAddr} )`)
-    console.log(`...tx: ${tx.transactionHash}`)
-}
+    const dstPingPongAddr = getDeploymentAddresses(taskArgs.targetNetwork)["PingPong"]
+    // get local contract instance
+    const pingPong = await ethers.getContract("PingPong")
+    console.log(`[source] pingPong.address: ${pingPong.address}`)
+

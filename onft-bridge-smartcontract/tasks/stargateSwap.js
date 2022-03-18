89b8f6b302077a338750c9884c18a39ecee32021
BabyDragon003 <golddragon01234@gmail.com>
2022-03-18 17:21:52 +0900

    "2022-03-18 17:21"

--- onft-bridge-smartcontract/tasks/stargateSwap.js
+++ onft-bridge-smartcontract/tasks/stargateSwap.js
@@ -13,6 +13,22 @@ module.exports = async function (taskArgs, hre) {
     const qty = taskArgs.qty
     const dstChainId = CHAIN_ID[taskArgs.targetNetwork]
     const dstStargateSwapAddr = getDeploymentAddresses(taskArgs.targetNetwork)["StargateSwap"]
+    // get source contract instance
+    const stargateSwap = await ethers.getContract("StargateSwap")
+    console.log(`[${hre.network.name}] StargateSwap: ${stargateSwap.address}`)
+    console.log(`[${taskArgs.targetNetwork}] StargateSwap: ${dstStargateSwapAddr}`)    
+
+    tx = await (await erc20.approve(stargateSwap.address, qty)).wait()
+    console.log(`[${hre.network.name}] approve tx: ${tx.transactionHash}`)
+
+    const stargateRouterAddress = await stargateSwap.stargateRouter()
+    console.log(`[${hre.network.name}] StargateRouter: ${stargateRouterAddress}`)
+    const stargateRouter = await ethers.getContractAt("IStargateRouter", stargateRouterAddress)
+    const quoteData = await stargateRouter.quoteLayerZeroFee(
+        dstChainId, 
+        1, // function type: see Bridge.sol for all types
+        owner.address, 
+        "0x", // payload
         {
             dstGasForCall: 20000, // extra gas, if calling smart contract,
             dstNativeAmount: 0,   // amount of dust dropped in destination wallet

46234c0dc3b42e7faab02b41f9a056f0180bd652
BabyDragon003 <golddragon01234@gmail.com>
2022-03-29 17:21:52 +0900

    "2022-03-29 17:21"

--- onft-bridge-smartcontract/tasks/deployAllChains.js
+++ onft-bridge-smartcontract/tasks/deployAllChains.js
@@ -1,13 +1,8 @@
+const shell = require('shelljs')
 const {TEST_CHAINS, CHAINS} = require('../constants/networks');
 
 module.exports = async function (taskArgs, hre) {
     let localContract, remoteContract;
-    let networks;
-    if (taskArgs.task == "test") {
-        networks = TEST_CHAINS;
-    } else {
-        networks = CHAINS;
-    }
 
     let nft_num = parseInt(taskArgs.nfts);
     let nft_startid = 1;

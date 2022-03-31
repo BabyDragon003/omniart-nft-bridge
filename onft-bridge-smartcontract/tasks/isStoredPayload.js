00347db08cb7a50ae0f9dee7eac469dbb435db55
BabyDragon003 <golddragon01234@gmail.com>
2022-03-31 17:21:52 +0900

    "2022-03-31 17:21"

--- onft-bridge-smartcontract/tasks/isStoredPayload.js
+++ onft-bridge-smartcontract/tasks/isStoredPayload.js
@@ -8,11 +8,8 @@ module.exports = async function (taskArgs, hre) {
     ];
 
     // console.log({taskArgs})
-        let payload = "0x" + taskArgs.payload
-        let tx = await (await endpoint.retryPayload(taskArgs.srcChainId, remoteAndLocal, payload)).wait()
-        console.log(`tx: ${tx.transactionHash}`)
-    }
-}
+    const endpoint = await ethers.getContractAt(EndpointAbi, ENDPOINTS[hre.network.name]);
 
-// npx hardhat storedPayload --network polygon --src-chain-id TBD --src-address TBD --des-address TBD
-// npx hardhat storedPayload --network polygon --src-chain-id 101 --src-address 0x165192f89ea752f597203eeb14e8f5538bce799d --des-address 0x9add6f279394f7f3c7a61d3426a7f45e149261a4
+    // concat remote and local address
+    let remoteAndLocal = hre.ethers.utils.solidityPack(
+        ['address','address'],

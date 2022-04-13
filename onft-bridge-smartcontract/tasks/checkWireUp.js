a1964d4bb1e8d617b39ea2e99da0d262f24c41e9
BabyDragon003 <golddragon01234@gmail.com>
2022-04-13 17:21:52 +0900

    "2022-04-13 17:21"

--- onft-bridge-smartcontract/tasks/checkWireUp.js
+++ onft-bridge-smartcontract/tasks/checkWireUp.js
@@ -3,6 +3,12 @@ const environments = require("../constants/environments.json")
 
 function TrustedRemoteTestnet() {
     this.goerli
+    this.bscTestnet
+    this.fuji
+    this.mumbai
+    this.arbitrumGoerli
+    this.optimismGoerli
+    this.fantomTestnet
 }
 
 function TrustedRemote() {
--- onft-bridge-smartcontract/utils/getAddresses.js
+++ onft-bridge-smartcontract/utils/getAddresses.js
@@ -3,16 +3,11 @@ const fs = require("fs")
 
 const environmentArg = process.argv[2]
 const contractCsvArg = process.argv[3]
-        const networks = environments[environment];
-        for (const network of networks) {
-            let fileName = `deployments/${network}/${contract[0].toUpperCase() + contract.substring(1)}.json`;
-            if(fs.existsSync(fileName)) {
-                promises.push(getAddressForNetwork(fileName, network))
-            }
-        }
-    }
-    const resolvedPromises = await Promise.all(promises)
-    resolvedPromises.forEach((networkAddressStr) => {
+
+async function getAddresses(environment, contractCsv) {
+    let contracts = contractCsv.split(",")
+    const promises = []
+    for (const contract of contracts) {
         console.log(networkAddressStr)
     })
 }

97e8aec6cca1cfa7bb47bfda4aa560f312967201
BabyDragon003 <golddragon01234@gmail.com>
2022-05-28 17:21:52 +0900

    "2022-05-28 17:21"

--- onft-bridge-smartcontract/contracts/interfaces/ILayerZeroUserApplicationConfig.sol
+++ onft-bridge-smartcontract/contracts/interfaces/ILayerZeroUserApplicationConfig.sol
@@ -13,3 +13,13 @@ interface ILayerZeroUserApplicationConfig {
     // @notice set the send() LayerZero messaging library version to _version
     // @param _version - new messaging library version
     function setSendVersion(uint16 _version) external;
+
+    // @notice set the lzReceive() LayerZero messaging library version to _version
+    // @param _version - new messaging library version
+    function setReceiveVersion(uint16 _version) external;
+
+    // @notice Only when the UA needs to resume the message flow in blocking mode and clear the stored payload
+    // @param _srcChainId - the chainId of the source chain
+    // @param _srcAddress - the contract address of the source contract at the source chain
+    function forceResumeReceive(uint16 _srcChainId, bytes calldata _srcAddress) external;
+}
